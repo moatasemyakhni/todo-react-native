d8ae2620be01b38fbd2d5088562ba16d5660d445
moatasem <motasem.yakhni@gmail.com>
2022-12-03 12:18:25 +0200

    Components => Button: style button

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    iconBtn: {
+        height: 60,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        borderRadius: 5,
+    },
+    iconText: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        color: 'white',
+        marginLeft: 10,
+    },
+});

205788868095d02dc564c50236d39cce49501c82
moatasem <motasem.yakhni@gmail.com>
2022-12-03 14:41:14 +0200

    App => import switcher stack in App

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import SwitcherStack from './src/navigation/SwitcherStack';
 
-export default function App() {
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
+
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <SwitcherStack/>
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+export default App;

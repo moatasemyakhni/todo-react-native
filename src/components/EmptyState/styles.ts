1950a1d9ec98a13e38fc00a84988e4cccaf03a6d
moatasem <motasem.yakhni@gmail.com>
2022-12-03 12:20:18 +0200

    Components => Empty_State: style component

---
+++ src/components/EmptyState/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#D8E3FF',
+    },
+    text: {
+        fontSize: 32,
+        textTransform: 'capitalize',
+        marginTop: 25,
+    }
+});

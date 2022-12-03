72bc11cdea6f280823144624139e8669d684f177
moatasem <motasem.yakhni@gmail.com>
2022-12-03 11:52:02 -0600

    Navigation => Main_Stack: import customized Drawer

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import Todo from '../screens/Todo';
+import StackHeader from '../components/StackHeader';
+import DrawerContent from '../components/DrawerContent';
 
+import { MaterialIcons } from '@expo/vector-icons';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const MainStack = () => {
     const StackMain = createStackNavigator();
-
+    const Drawer = createDrawerNavigator();
     return (
-        <StackMain.Navigator>
-            <StackMain.Screen
+        <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
+            <Drawer.Screen
                 name='Todo'
                 component={Todo}
+                options={{ 
+                    headerTitle: (props) => <StackHeader 
+                    text={'MyTodos'} 
+                    Icon={ () => <MaterialIcons name='assignment' size={29}/>}
+                    {...props}/> 
+                 }}
             />
-        </StackMain.Navigator>
+        </Drawer.Navigator>
     );
 }
 

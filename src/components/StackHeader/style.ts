58a6aa7a0ebf34fa0ff5cc744466a837e2305b0e
moatasem <motasem.yakhni@gmail.com>
2022-12-03 05:51:58 -0600

    Component => StackHeader: style component

---
+++ src/components/StackHeader/style.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: { 
+        width:'100%', 
+        flexDirection: 'row', 
+        alignItems:'center', 
+        justifyContent: 'center',  
+    },
+    text: { 
+        fontSize: 32, 
+        fontFamily: 'bad-script', 
+        marginLeft: 10 
+    },
+});

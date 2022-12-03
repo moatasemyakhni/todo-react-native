7972fa4faca99529f3bf6337ae52367c0ace2eb1
moatasem <motasem.yakhni@gmail.com>
2022-12-03 15:21:40 -0600

    Redux => Store: import todos Reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,13 @@
 import userReducer from './slices/usersSlice';
+import todosReducer from './slices/todosSlice';
 
 import { configureStore } from '@reduxjs/toolkit';
+
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
+        todos: todosReducer,
     },
 });
 

cd62211a77c558250063f8ab9a5b3285fdf91d78
moatasem <motasem.yakhni@gmail.com>
2022-12-03 14:38:32 +0200

    Navigation => Switcher_Stack: remove Stack navigator

--- src/navigation/SwitcherStack.tsx
+++ src/navigation/SwitcherStack.tsx
@@ -4,13 +4,11 @@ import MainStack from './MainStack';
 
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
-import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 
 const SwitcherStack = () => {
     const user = useSelector((state: RootState) => state.user);
-    const Stack = createStackNavigator();
 
     return (
         <NavigationContainer>

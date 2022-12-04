7a4f2de0c22defb13612034983489988f1e56961
moatasem <motasem.yakhni@gmail.com>
2022-12-04 11:14:58 +0200

    Component => ErrorMessage: style app

---
+++ src/components/ErrorMessage/style.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    error: { 
+        backgroundColor: 'rgba(255,114,118, 0.3)', 
+        padding: 10, 
+        marginTop: 10, 
+        textAlign: 'center', 
+        color: 'red' 
+    }
+});

d08d8347154550d1952d1dbd1df302215bee843d
moatasem <motasem.yakhni@gmail.com>
2022-12-04 11:18:07 +0200

    Screen => Signup: remove errorMessage style

--- src/screens/Signup/style.ts
+++ src/screens/Signup/style.ts
@@ -62,11 +62,4 @@ export const styles = StyleSheet.create({
     btnText: {
         textTransform: 'uppercase'
     },
-    error: { 
-        backgroundColor: 'rgba(255,114,118, 0.3)', 
-        padding: 10, 
-        marginTop: 10, 
-        textAlign: 'center', 
-        color: 'red' 
-    }
 });

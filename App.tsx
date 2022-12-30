fe98cad39b7e79ab774bee2f5539130f040a53ef
moatasem <motasem.yakhni@gmail.com>
2022-12-31 00:46:09 +0200

    App => switch from redux to context provider

--- App.tsx
+++ App.tsx
@@ -3,8 +3,6 @@ import 'expo-dev-client';
 
 import React from 'react';
 import SwitcherStack from './src/navigation/SwitcherStack';
-import { Provider } from 'react-redux';
-import { store } from './src/redux/store';
 import UserProvider from './src/providers/UserProvider';
 
 

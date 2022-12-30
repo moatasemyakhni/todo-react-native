4a44613bd61085565b207b569afe0600dcf8d6f7
moatasem <motasem.yakhni@gmail.com>
2022-12-31 00:45:39 +0200

    App => wrap SwitcherStack component with the User Provider

--- App.tsx
+++ App.tsx
@@ -5,13 +5,14 @@ import React from 'react';
 import SwitcherStack from './src/navigation/SwitcherStack';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
+import UserProvider from './src/providers/UserProvider';
 
 
 const App = () => {
   return (
-    <Provider store={store}>
+    <UserProvider>
       <SwitcherStack/>
-    </Provider>
+    </UserProvider>
   );
 }
 

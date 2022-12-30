97f6399d2ebc81c175805228d33b94e506a0a572
moatasem <motasem.yakhni@gmail.com>
2022-12-31 00:50:17 +0200

    Navigation => Switcher_Stack: destructure userContext and get the user state

--- src/navigation/SwitcherStack.tsx
+++ src/navigation/SwitcherStack.tsx
@@ -1,21 +1,24 @@
 import 'react-native-gesture-handler';
+
 import React from 'react';
 import AuthStack from './AuthStack';
 import MainStack from './MainStack';
+import TodoProvider from '../providers/TodoProvider';
+import UserContextAPI from '../context/UserContextAPI';
 
-import { useSelector } from 'react-redux';
-import { RootState } from '../redux/store';
+import { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 
 const SwitcherStack = () => {
-    const user = useSelector((state: RootState) => state.user);
-
+    const [user] = useContext(UserContextAPI);
+    
     return (
         <NavigationContainer>
             {user.currentUser?.id?
-                <MainStack />
+                <TodoProvider><MainStack /></TodoProvider>
             :
+            // UserProvide is already wrapping the SwitcherStack
                 <AuthStack />
             }
         </NavigationContainer>
